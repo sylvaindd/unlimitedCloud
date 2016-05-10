9b093d3ba3f005d7903422e40953ebba91e4a803
thelittlefireman <thomas.goureau@gmail.com>
2016-05-10 16:29:18 +0200

    set Interfaces

---
+++ server/ultimateCloudServer/src/main/java/com/ultimateCloud/App/cloudServices/Dropbox.java
@@ -0,0 +1,12 @@
+package com.ultimateCloud.App.cloudServices;
+
+import com.ultimateCloud.App.interfaces.CloudServiceInterface;
+
+/**
+ * Created by thoma on 10/05/2016.
+ */
+public class Dropbox implements CloudServiceInterface {
+    public void authentihication() {
+
+    }
+}
---
+++ server/ultimateCloudServer/src/main/java/com/ultimateCloud/App/interfaces/CloudServiceInterface.java
@@ -0,0 +1,8 @@
+package com.ultimateCloud.App.interfaces;
+
+/**
+ * Created by thoma on 10/05/2016.
+ */
+public interface CloudServiceInterface {
+    public void authentihication();
+}
