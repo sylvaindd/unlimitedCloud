ce3b121755124a87d2e877e8942f7e89e8655cb9
thelittlefireman <thomas.goureau@gmail.com>
2016-05-10 17:52:32 +0200

    get filesListes

--- server/ultimateCloudServer/src/main/java/com/ultimateCloud/App/interfaces/CloudServiceInterface.java
+++ server/ultimateCloudServer/src/main/java/com/ultimateCloud/App/interfaces/CloudServiceInterface.java
@@ -1,9 +1,14 @@
 package com.ultimateCloud.App.interfaces;
 
+import com.ultimateCloud.App.models.FileCloud;
+
+import java.net.URI;
+import java.util.List;
+
 /**
  * Created by thoma on 10/05/2016.
  */
 public interface CloudServiceInterface {
-    public final static String apiBaseUrl="";
-    public void getFileList();
+    public String getBaseURI();
+    public List<FileCloud> getFileList();
 }
---
+++ server/ultimateCloudServer/src/main/java/com/ultimateCloud/App/models/FileCloud.java
@@ -0,0 +1,7 @@
+package com.ultimateCloud.App.models;
+
+/**
+ * Created by thoma on 10/05/2016.
+ */
+public class FileCloud {
+}
