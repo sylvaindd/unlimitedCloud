f96af2fcc75779da5acc814999d199b8fde36471
thelittlefireman <thomas.goureau@gmail.com>
2016-05-10 17:51:08 +0200

    get filesListes

--- server/ultimateCloudServer/src/main/java/com/ultimateCloud/App/cloudServices/GoogleDrive.java
+++ server/ultimateCloudServer/src/main/java/com/ultimateCloud/App/cloudServices/GoogleDrive.java
@@ -6,6 +6,11 @@ import com.ultimateCloud.App.interfaces.CloudServiceInterface;
  * Created by thoma on 10/05/2016.
  */
 public class GoogleDrive implements CloudServiceInterface {
+
+    public String getBaseURI() {
+        return null;
+    }
+
     public void getFileList() {
 
     }
