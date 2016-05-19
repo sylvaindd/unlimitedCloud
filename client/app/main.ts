4218217071e7f4fca21cffb690daaf0e4b027842
guillaumectd <guigui.cot35@gmail.com>
2016-05-19 10:23:13 +0200

    rooting connection

--- client/app/main.ts
+++ client/app/main.ts
@@ -1,9 +1,12 @@
 import {bootstrap} from '@angular/platform-browser-dynamic'
-import {HTTP_PROVIDERS} from '@angular/http'; // Dependencies for HTTP service
+import {HTTP_PROVIDERS} from '@angular/http';
+import { ROUTER_PROVIDERS } from 'angular2/router';
+// Dependencies for HTTP service
 import 'rxjs/Rx'; // For using methods on observables
 import {App} from "./App";
 import {Connection} from "./Connection";
 
+bootstrap(App, [ROUTER_PROVIDERS]);
 bootstrap(App, [HTTP_PROVIDERS]);
 bootstrap(Connection, [HTTP_PROVIDERS]);
 
