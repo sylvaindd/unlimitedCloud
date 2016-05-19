fb63bce0980b057fca26cf18fe58737768c01522
thelittlefireman <thomas.goureau@gmail.com>
2016-05-19 09:51:13 +0200

    jdbc

--- server/ultimateCloudServer/src/main/java/com/ultimateCloud/App/jdbc/JDBCMysSQL.java
+++ server/ultimateCloudServer/src/main/java/com/ultimateCloud/App/jdbc/JDBCMysSQL.java
@@ -27,6 +27,93 @@ public class JDBCMysSQL {
         }
     }
 
+    public void addDropBoxTokenToOurAccount(String token,String ourToken){
+        //TODO
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            stmt = conn.createStatement();
+
+            if (stmt.execute("SELECT foo FROM bar")) {
+                rs = stmt.getResultSet();
+            }
+
+            // Now do something with the ResultSet ....
+        }
+        catch (SQLException ex){
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+        finally {
+            // it is a good idea to release
+            // resources in a finally{} block
+            // in reverse-order of their creation
+            // if they are no-longer needed
+
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException sqlEx) { } // ignore
+
+                rs = null;
+            }
+
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException sqlEx) { } // ignore
+
+                stmt = null;
+            }
+        }
+    }
+
+    public void addDropBoxCodeToOurAccount(String code,String ourToken){
+        //TODO
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            stmt = conn.createStatement();
+
+            if (stmt.execute("SELECT foo FROM bar")) {
+                rs = stmt.getResultSet();
+            }
+
+            // Now do something with the ResultSet ....
+        }
+        catch (SQLException ex){
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+        finally {
+            // it is a good idea to release
+            // resources in a finally{} block
+            // in reverse-order of their creation
+            // if they are no-longer needed
+
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException sqlEx) { } // ignore
+
+                rs = null;
+            }
+
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException sqlEx) { } // ignore
+
+                stmt = null;
+            }
+        }
+    }
     public void getDropBoxAccountList(){
         Statement stmt = null;
         ResultSet rs = null;
