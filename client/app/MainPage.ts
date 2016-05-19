9ebe04377023bcf9bbf7a4b07f8684b086169798
guillaumectd <guigui.cot35@gmail.com>
2016-05-19 15:47:57 +0200

    redirection ok

--- client/app/MainPage.ts
+++ client/app/MainPage.ts
@@ -6,13 +6,16 @@ import {FirstPage} from "./FirstPage";
 @Component({
     selector: "mainPage",
     template: `
+<button (click)="onSubmit()">Test</button>
 <router-outlet></router-outlet>`,
     directives: [ROUTER_DIRECTIVES]
 })
 
 @Routes([
     {path: '/', component: FirstPage},
-    {path: '/App', component: App}
+    {path: '/App', component: App},
+    {path: '/App/Explorer', component: App},
+    {path: '/App/Settings', component: App}
 ])
 
 export class MainPage {
