04ef403cecc02e1164c2f10a2c2f28885828324f
guillaumectd <guigui.cot35@gmail.com>
2016-05-19 09:22:03 +0200

    begginig of explorer

---
+++ client/app/drawer.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: "drawer",
+    templateUrl: "app/drawer.html"
+})
+export class Drawer {
+
+    constructor(){
+
+
+    }
+}
