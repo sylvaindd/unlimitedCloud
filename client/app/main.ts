b494aef2011c132f630ecc69085471a939540a93
Sylvain <sylvainduarte@hotmail.fr>
2016-05-19 09:37:36 +0200

    Angular

--- client/app/main.ts
+++ client/app/main.ts
@@ -1,9 +1,10 @@
-import {bootstrap} from '@angular/platform-browser-dynamic'
+/// <reference path="../node_modules/angular2/typings/browser.d.ts" />
+
+import {bootstrap} from '@angular/platform-browser-dynamic';
 import {HTTP_PROVIDERS} from '@angular/http'; // Dependencies for HTTP service
 import 'rxjs/Rx'; // For using methods on observables
 import {App} from "./App";
 import {Connection} from "./Connection";
 
 bootstrap(App, [HTTP_PROVIDERS]);
-bootstrap(Connection, [HTTP_PROVIDERS]);
 
